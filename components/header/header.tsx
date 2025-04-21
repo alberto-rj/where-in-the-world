4f721d56c9772d0578428099d35f176ad5353c01
Alberto Raúl José <albertorauljose2@gmail.com>
2025-04-21 16:36:04 +0000

    feat: create header component

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,7 +3,7 @@ import { ThemePicker } from '@/components/theme-picker';
 
 export const Header = () => {
 	return (
-		<header className='bg-neutral-100 dark:bg-neutral-800 py-8'>
+		<header className='bg-neutral-100 dark:bg-neutral-1 shadow-md py-8'>
 			<div className='container flex justify-between items-center'>
 				<Logo />
 				<ThemePicker />
